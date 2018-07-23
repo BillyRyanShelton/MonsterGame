3bba07170eafb76ccb127a0d968025916e53cea8
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 13:38:36 -0500

    Create Monster.hpp

---
+++ Monster.hpp
@@ -0,0 +1,37 @@
+/*********************************************************************
+** Author:		Billy R. Shelton
+** Date:		7/23/2018
+** Description:  This is a header file for the base class Monster.
+*********************************************************************/
+
+#ifndef MONSTER_HPP
+#define MONSTER_HPP
+#include <string>
+
+class Monster
+{
+	protected:
+		int attack;
+		int defense;
+		int armor;
+		int strength;
+		std::string name;
+	public:
+		Monster();
+		Monster(int, int, int, int);
+		int getAttack();
+		void setAttack(int);
+		int getDefense();
+		void setDefense(int);
+		int getArmor();
+		std::string getName();
+		void setArmor(int);
+		void setStrength(int);
+		int getStrength();
+		virtual int attackPlayer();
+		virtual int defend();
+		virtual void checkStrength();
+};
+
+
+#endif
