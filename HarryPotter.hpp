577090d2f028af3b58ae2456e92af8f334fa5235
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 13:36:05 -0500

    Create HarryPotter.hpp

---
+++ HarryPotter.hpp
@@ -0,0 +1,22 @@
+/*********************************************************************
+** Author:		Billy R. Shelton
+** Date:		7/23/2018
+** Description: This is a header file for the derived class HarryPotter
+*********************************************************************/
+
+#ifndef HARRYPOTTER_HPP
+#define HARRYPOTTER_HPP
+#include "Monster.hpp"
+#include <string>
+
+class HarryPotter :public Monster
+{
+	private:
+		int livesLeft = 1;
+
+	public:
+		void checkStrength();
+		HarryPotter();
+		HarryPotter(int, int, int, int);
+};
+#endif
