80f2e60640bf72ac89fc3ff04116f4fcf9d37714
BillyRyanShelton <35415026+BillyRyanShelton@users.noreply.github.com>
2018-07-23 13:34:13 -0500

    Create Barbarian.hpp

---
+++ Barbarian.hpp
@@ -0,0 +1,18 @@
+/*********************************************************************
+** Author:		Billy R. Shelton
+** Date:		7/23/2018
+** Description:  This is a header file for the derived class Barbarian.
+*********************************************************************/
+
+#ifndef BARBARIAN_HPP
+#define BARBARIAN_HPP
+#include "Monster.hpp"
+#include <string>
+
+class Barbarian :public Monster
+{
+	public:
+		Barbarian();
+		Barbarian(int, int, int, int);
+};
+#endif
